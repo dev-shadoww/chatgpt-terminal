49b103db8a763a0bd5df7a6b385e3b3d69e0a237
dev-shadoww <dev.shadoww@gmail.com>
2023-05-20 22:13:48 +0530

    commit 4, Add file save feature and fix line break bug

--- main.py
+++ main.py
@@ -1,28 +1,25 @@
-# By Suraj Kareppagol
-# GitHub : https://github.com/dev-shadoww
-
-# Use your own API key to use this program.
-# Other packages used are, openai, rich (for styling the terminal), json
-
+import os
 import openai
 import requests
-from rich.prompt import Prompt as rich_input
-from rich import print as rich_print
-from rich.panel import Panel
 import json
+from dotenv import load_dotenv
 
-from rich.prompt import Prompt
+from rich.console import Console
+from rich.panel import Panel
 
-rich_print(Panel.fit("ChatGPT, On Terminal", title_align="center"))
+load_dotenv()
 
-rich_print("Enter your query below, to quit enter [red]\"exit\",")
-
-openai.api_key = "OPEN_AI_KEY"
+console = Console(record=True)
+openai.api_key = os.getenv("OPEN_AI_API_KEY")
 api_url = "https://api.openai.com/v1/chat/completions"
-headers_object = {"Authorization": f"Bearer {openai.api_key}"}
+api_headers = {"Authorization": f"Bearer {openai.api_key}"}
+
+console.print(
+    Panel.fit("[bold red]ChatGPT, On Terminal[/bold red]", title_align="center"))
+console.print("Enter your query below, to quit enter [red]\"exit\",")
 
 while True:
-    user_query = rich_input.ask("[bold green]User   [/bold green]")
+    user_query = console.input("[bold green]User   [/bold green]: ")
     if (user_query.lower() == "exit"):
         break
 
@@ -32,8 +29,15 @@ while True:
     }
 
     response = requests.post(
-        api_url, json=request_object, headers=headers_object)
+        api_url, json=request_object, headers=api_headers)
     response_object = json.loads(response.text)
-    rich_print("[bold blue]ChatGPT[/bold blue]: ", end='')
-    rich_print(response_object["choices"][0]["message"]["content"])
-    rich_print()
+    console.print("[bold blue]ChatGPT[/bold blue]: ", end='')
+    console.print(response_object["choices"][0]
+                  ["message"]["content"], soft_wrap=True)
+    console.print()
+
+export_text_file = console.input(
+    "Do you want to export your conversation into a file? [bold green](Y or N)[/bold green]: ")
+if (export_text_file.lower() == 'y'):
+    with open("conversation.txt", 'w') as file:
+        file.write(console.export_text(clear=True))
